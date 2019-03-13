ee7e767547e738420a9d49f7f940bfb4dc3c5a31
John van de Vrugt <johnnywasp@msn.com>
2019-03-13 21:15:50 +0100

    Centralized configuration

---
+++ devices/configuration.py
@@ -0,0 +1,25 @@
+class Configuration:
+    STD_UNIT_POWER = 1
+    STD_UNIT_GAS = 2
+    STD_UNIT_TEMPERATURE = 3
+    STD_UNIT_SET_POINT = 4
+    STD_UNIT_HEATING_ACTIVE = 5
+    STD_UNIT_HOT_WATER_ACTIVE = 6
+    STD_UNIT_PREHEAT_ACTIVE = 7
+    STD_UNIT_SCENE = 8
+    STD_UNIT_PROGRAM_STATE = 9
+    STD_UNIT_MODULATION_LEVEL = 10
+
+    STR_UNIT_POWER = "Power usage"
+    STR_UNIT_GAS = "Gas usage"
+    STR_UNIT_TEMPERATURE = "Room temperature"
+    STR_UNIT_SET_POINT = "Set point"
+    STR_UNIT_HEATING_ACTIVE = "Heating active"
+    STR_UNIT_HOT_WATER_ACTIVE = "Hot water active"
+    STR_UNIT_PREHEAT_ACTIVE = "Preheat active"
+    STR_UNIT_SCENE = "Scene"
+    STR_UNIT_PROGRAM_STATE = "Program state"
+    STR_UNIT_MODULATION_LEVEL = "Modulation level"
+
+
+config = Configuration()
